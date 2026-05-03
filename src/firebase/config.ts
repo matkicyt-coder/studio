fa470e31a22e0998790874bb75aa042d4b7db0e2
Matija Filipovic <matkicme@gmail.com>
2026-05-03 07:08:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2794390342-1bcd9",
+  "appId": "1:460429988043:web:1fb8097984f2beb6b98b4e",
+  "apiKey": "AIzaSyBOhJzx9O78iQ8NlIHhqGhUtM2ZDhKrmyk",
+  "authDomain": "studio-2794390342-1bcd9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "460429988043"
+};
